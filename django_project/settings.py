4af4d5602c73a7dd0dc4cd80fb61257a1c3b7042
Jawdat Tayfour <18jwdat@gmail.com>
2023-08-01 19:14:24 +0300

    ch7

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     #local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    # Third-party
+    "crispy_forms", # new
+    "crispy_bootstrap5", # new
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" # new
+CRISPY_TEMPLATE_PACK = "bootstrap5" # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- pages/tests.py
+++ pages/tests.py
@@ -2,7 +2,7 @@ from django.test import SimpleTestCase
 from django.urls import reverse , resolve
 
 
-from .views import HomePageView
+from .views import HomePageView , AboutPageView
 
 class HomePageTests(SimpleTestCase):
 
@@ -27,3 +27,27 @@ class HomePageTests(SimpleTestCase):
     def test_hompage_url_resolves_HomePageView(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__,HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self) -> None:
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_url_exist_at_correct_location(self):
+        self.assertEqual(self.response.status_code,200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response,'about.html')
+
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response,'About')
+
+        
+    def test_homepage_does_not_contains_incorrect_html(self):
+        self.assertNotContains(self.response,'I don"t belong here!')
+
+    def test_hompage_url_resolves_HomePageView(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__,AboutPageView.as_view().__name__)
